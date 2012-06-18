25fb3e4b434fa19956527c8a22480d7c0bc96d92
netarchy <netarchy@gmail.com>
2012-06-18 14:20:39 -0500

    Corrections to voltage interface for the expanded cpu frequency table

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -883,7 +883,7 @@ ssize_t store_UV_uV_table(struct cpufreq_policy *policy,
 	int i = 0;
 	int t[17];
 
-	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
 		     &t[0],&t[1],&t[2],&t[3],&t[4],&t[5],&t[6],&t[7],&t[8],&t[9],&t[10],&t[11],&t[12],&t[13],&t[14],&t[15],&t[16]);
 
 	if(ret != 17) {
@@ -913,7 +913,7 @@ ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
 	int i = 0;
 	int t[17];
 
-	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
 		     &t[0],&t[1],&t[2],&t[3],&t[4],&t[5],&t[6],&t[7],&t[8],&t[9],&t[10],&t[11],&t[12],&t[13],&t[14],&t[15],&t[16]);
 
 	if(ret != 17) {
